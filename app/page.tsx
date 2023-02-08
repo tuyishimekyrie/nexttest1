38ed7ee237a655ebc5a60d462eab630af3b3e42a
kyrie <tuyishimekyrie@gmail.com>
2023-02-08 13:34:05 +0200

    fourth commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className="bg-indigo-800 min-h-screen text-white text-center py-40 text-5xl">
-    <p>Next js 13</p></div>
+      <p>Next js {process.env.NEXT_PUBLIC_VERSION}</p></div>
   )
 }
 
