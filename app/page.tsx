f908acfd004411db6ccf2891c16286a767871748
kyrie <tuyishimekyrie@gmail.com>
2023-02-08 13:23:37 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="bg-indigo-800 min-h-screen text-white text-center text-4xl">Nextjs 13</div>
+  )
+}
+
+export default page
